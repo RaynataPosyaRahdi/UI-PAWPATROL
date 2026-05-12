f15f5a05427cfd347b8e758a30aaf3443e1c4888
muhamad rizal <mrizalahyarta@gmail.com>
2026-05-12 14:43:05 +0700

    Menambahkan model aplikasi pawpatrol

--- app/src/main/java/com/example/pawpatrol/model/Hewan.kt
+++ app/src/main/java/com/example/pawpatrol/model/Hewan.kt
@@ -1,18 +1,11 @@
 package com.example.pawpatrol.model
 
-enum class StatusHewan {
-    TERSEDIA,
-    DIADOPSI
-}
-
 data class Hewan(
     val id: Int,
     val namaHewan: String,
     val umur: Int,
     val foto: String?,
-    val status: StatusHewan,
-
-    // Foreign Key
+    val status: String,
     val idKategori: Int,
     val idUser: Int?
 )
--- app/src/main/java/com/example/pawpatrol/model/Transaksi.kt
+++ app/src/main/java/com/example/pawpatrol/model/Transaksi.kt
@@ -1,20 +1,11 @@
 package com.example.pawpatrol.model
 
-enum class StatusPembayaran {
-    PENDING,
-    LUNAS,
-    GAGAL
-}
-
 data class Transaksi(
     val id: Int,
-
-    // Foreign Key
     val idUser: Int,
     val idProduk: Int?,
     val idGrooming: Int?,
-
     val tanggal: String,
     val total: Int,
-    val statusPembayaran: StatusPembayaran
+    val statusPembayaran: String
 )
