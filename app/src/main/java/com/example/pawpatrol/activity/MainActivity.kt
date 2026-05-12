1a2045c80722fa2915890606c44a6b87c856fa81
sakhra <sakhramawla@gmail.com>
2026-05-12 15:19:55 +0700

    Menambahkan syntax pada MainActivity dan halaman booking grooming

--- app/src/main/java/com/example/pawpatrol/activity/MainActivity.kt
+++ app/src/main/java/com/example/pawpatrol/activity/MainActivity.kt
@@ -1,2 +1,19 @@
 package com.example.pawpatrol.activity
 
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // pindah ke dashboard
+        startActivity(
+            Intent(this, DashboardActivity::class.java)
+        )
+
+        finish()
+    }
+}
