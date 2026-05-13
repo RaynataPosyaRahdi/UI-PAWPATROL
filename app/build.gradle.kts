3ed7c4b3fcf9b0dbdc5348728ab8ead96e7e94db
RaynataPosyaRahdi <224443066@mhs.polman-bandung.ac.id>
2026-05-13 13:48:40 +0700

    Membuat dan memperbarui layout halaman login dan register

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,7 @@ android {
 }
 
 dependencies {
+
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
@@ -55,7 +56,30 @@ dependencies {
 
     // CardView
     implementation("androidx.cardview:cardview:1.0.0")
-
+    implementation("com.google.android.material:material:1.12.0")
     // Material Design
-    implementation("com.google.android.material:material:1.11.0")
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("androidx.cardview:cardview:1.0.0")
+    implementation("com.google.android.material:material:1.12.0")
+
+    implementation("androidx.viewpager2:viewpager2:1.1.0")
+    testImplementation(libs.junit)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(libs.androidx.junit)
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+
 }
